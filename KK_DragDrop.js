a2da5629d0afc684daf52618201b044ae60ffb36
Kevin <HeadbuttStudios@gmail.com>
2021-03-05 11:53:13 -0800

    Initial commit

---
+++ KK_DragDrop.js
@@ -0,0 +1,348 @@
+let hiragana = [
+	{
+		src: 'images/あ.png',
+		jval: 'a',
+		id: 'jval-a',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/い.png',
+		jval: 'i',
+		id: 'jval-i',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/う.png',
+		jval: 'u',
+		id: 'jval-u',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/え.png',
+		jval: 'e',
+		id: 'jval-e',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/お.png',
+		jval: 'o',
+		id: 'jval-o',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/か.png',
+		jval: 'ka',
+		id: 'jval-ka',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/き.png',
+		jval: 'ki',
+		id: 'jval-ki',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/く.png',
+		jval: 'ku',
+		id: 'jval-ku',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/け.png',
+		jval: 'ke',
+		id: 'jval-ke',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/こ.png',
+		jval: 'ko',
+		id: 'jval-ko',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/さ.png',
+		jval: 'sa',
+		id: 'jval-sa',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/し.png',
+		jval: 'shi',
+		id: 'jval-shi',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/す.png',
+		jval: 'su',
+		id: 'jval-su',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/せ.png',
+		jval: 'se',
+		id: 'jval-se',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/そ.png',
+		jval: 'so',
+		id: 'jval-so',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/た.png',
+		jval: 'ta',
+		id: 'jval-ta',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ち.png',
+		jval: 'chi',
+		id: 'jval-chi',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/つ.png',
+		jval: 'tsu',
+		id: 'jval-tsu',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/て.png',
+		jval: 'te',
+		id: 'jval-te',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/と.png',
+		jval: 'to',
+		id: 'jval-to',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/な.png',
+		jval: 'na',
+		id: 'jval-na',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/に.png',
+		jval: 'ni',
+		id: 'jval-ni',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ぬ.png',
+		jval: 'nu',
+		id: 'jval-nu',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ね.png',
+		jval: 'ne',
+		id: 'jval-ne',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/の.png',
+		jval: 'no',
+		id: 'jval-no',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/は.png',
+		jval: 'ha',
+		id: 'jval-ha',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ひ.png',
+		jval: 'hi',
+		id: 'jval-hi',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ふ.png',
+		jval: 'fu',
+		id: 'jval-fu',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/へ.png',
+		jval: 'he',
+		id: 'jval-he',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ほ.png',
+		jval: 'ho',
+		id: 'jval-ho',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ら.png',
+		jval: 'ra',
+		id: 'jval-ra',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/り.png',
+		jval: 'ri',
+		id: 'jval-ri',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/る.png',
+		jval: 'ru',
+		id: 'jval-ru',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/れ.png',
+		jval: 're',
+		id: 'jval-re',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ろ.png',
+		jval: 'ro',
+		id: 'jval-ro',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ま.png',
+		jval: 'ma',
+		id: 'jval-ma',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/み.png',
+		jval: 'mi',
+		id: 'jval-mi',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/む.png',
+		jval: 'mu',
+		id: 'jval-mu',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/め.png',
+		jval: 'me',
+		id: 'jval-me',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/も.png',
+		jval: 'mo',
+		id: 'jval-mo',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/や.png',
+		jval: 'ya',
+		id: 'jval-ya',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ゆ.png',
+		jval: 'yu',
+		id: 'jval-yu',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/よ.png',
+		jval: 'yo',
+		id: 'jval-yo',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/わ.png',
+		jval: 'wa',
+		id: 'jval-wa',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/を.png',
+		jval: 'wo',
+		id: 'jval-wo',
+		class: 'j-char-img',
+	},
+	{
+		src: 'images/ん.png',
+		jval: 'n',
+		id: 'jval-n',
+		class: 'j-char-img',
+	},
+];
+
+// result is the array being shuffled
+let result = shuffle(hiragana);
+let section = document.getElementById('j-characters');
+// this loop initalizes the elements and their attributes.
+for (const character of result) {
+	let image = document.createElement('img');
+	image.src = character.src;
+	image.setAttribute('data-jval', character.jval);
+	image.setAttribute('id', character.id);
+	image.setAttribute('class', character.class);
+	image.setAttribute('draggable', 'true');
+	image.addEventListener('dragstart', drag);
+	let wrapper = document.createElement('div');
+	wrapper.appendChild(image);
+	section.appendChild(wrapper);
+}
+
+// below is the drag and drop code
+function drag(event) {
+	let data = event.target.dataset.jval;
+	event.dataTransfer.setData('text', data);
+}
+
+function allowDrop(event) {
+	event.preventDefault();
+}
+
+function drop(event) {
+	event.preventDefault();
+	var data = event.dataTransfer.getData('text');
+
+	let reference = document.getElementById(event.target.id);
+	let romanCharacter = reference.nextSibling.nextSibling.dataset.roman;
+	// This checks if it's the cooresponding character, plays a sound, and changes the bottom image for 1 second.
+	if (data === romanCharacter) {
+		console.log('correct');
+		event.target.appendChild(document.getElementById(`jval-${data}`));
+
+		// 	document.getElementById('myImage').src = src = '/images/right.png';
+		// 	play_single_sound('audiotag2');
+		// 	setTimeout(reset, 1000);
+		// } else {
+		// 	document.getElementById('myImage').src = src = '/images/wrong.png';
+		// 	play_single_sound('audiotag1');
+		// 	setTimeout(reset, 1000);
+	}
+}
+
+// this function will reset to default image.
+// function reset() {
+// 	document.getElementById('myImage').src = src = '/images/neutral.png';
+// }
+// this function will play a sound.
+// function play_single_sound(song) {
+// 	document.getElementById(song).play();
+// }
+// Fisher Yates Algorithm to shuffle our array.
+function shuffle(array) {
+	var m = array.length,
+		t,
+		i;
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+	return array;
+}
